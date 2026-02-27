c425cd0f4ac6ab43ee7003ae026b73ed89161384
Alina Svintsova <aline.svintsova@gmail.com>
2026-02-27 13:09:27 +0700

    update stream reader

--- TestTask/ReadOnlyStream.cs
+++ TestTask/ReadOnlyStream.cs
@@ -1,34 +1,29 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace TestTask
 {
     public class ReadOnlyStream : IReadOnlyStream
     {
-        private Stream _localStream;
+        private StreamReader _localStream;
 
         /// <summary>
         /// Конструктор класса. 
-        /// Т.к. происходит прямая работа с файлом, необходимо 
-        /// обеспечить ГАРАНТИРОВАННОЕ закрытие файла после окончания работы с таковым!
         /// </summary>
         /// <param name="fileFullPath">Полный путь до файла для чтения</param>
         public ReadOnlyStream(string fileFullPath)
         {
-            IsEof = true;
-
-            // TODO : Заменить на создание реального стрима для чтения файла!
-            _localStream = null;
+            var fileStream = File.Open(fileFullPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            _localStream = new StreamReader(fileStream, encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: true);
+            if(_localStream == null)
+                throw new EndOfStreamException();
         }
-                
+
         /// <summary>
         /// Флаг окончания файла.
         /// </summary>
-        public bool IsEof
-        {
-            get; // TODO : Заполнять данный флаг при достижении конца файла/стрима при чтении
-            private set;
-        }
+        public bool IsEof => _localStream == null || _localStream.EndOfStream;
 
         /// <summary>
         /// Ф-ция чтения следующего символа из потока.
@@ -38,8 +33,14 @@ namespace TestTask
         /// <returns>Считанный символ.</returns>
         public char ReadNextChar()
         {
-            // TODO : Необходимо считать очередной символ из _localStream
-            throw new NotImplementedException();
+            try
+            {
+                 return (char)_localStream.Read();
+            }
+            catch (EndOfStreamException e)
+            {
+                throw new EndOfStreamException(e.Message);
+            }
         }
 
         /// <summary>
@@ -49,12 +50,17 @@ namespace TestTask
         {
             if (_localStream == null)
             {
-                IsEof = true;
                 return;
             }
 
-            _localStream.Position = 0;
-            IsEof = false;
+            _localStream.DiscardBufferedData();
+            _localStream.BaseStream.Position = 0;
+        }
+
+        public void Dispose()
+        {
+            _localStream?.Dispose();
+            _localStream = null;
         }
     }
 }
